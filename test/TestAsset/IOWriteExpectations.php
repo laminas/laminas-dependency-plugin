2c698a6f34ae462092a1042c7c9242c54afef875
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-09-07 11:47:45 -0500

    qa: apply laminas-coding-standard 2.2 rules
    Primarily removal of file-level docblocks.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/TestAsset/IOWriteExpectations.php
+++ test/TestAsset/IOWriteExpectations.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-dependency-plugin for the canonical source repository
- * @copyright https://github.com/laminas/laminas-dependency-plugin/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-dependency-plugin/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace LaminasTest\DependencyPlugin\TestAsset;
